a19817729187f70db93f5b9b244614d0c93710e0
kirl <kirl@ya.ru>
2025-09-05 11:16:29 +0300

    .

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.Scope;
+
+//@Scope("prototype")
+public class Cat {
+    private String message;
+
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+}
